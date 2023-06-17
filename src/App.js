cd4eb607e1694c6b6683c6ad7a49080657d65778
Glauber Apolinario <glauber.apolinario@yahoo.com.br>
2023-06-17 15:19:02 -0300

    Limpeza de código

--- src/App.js
+++ src/App.js
@@ -15,22 +15,20 @@ function App() {
         prompt: e.target.value,
         max_tokens: 56,
         temperature: 0,
-      }
+      };
       client
         .post("https://api.openai.com/v1/completions", params)
         .then((result) => setAnswer(result.data.choices[0].text))
         .catch((err) => console.log(err))
-    }
-  }
+    };
+  };
 
   return (
     <div className="App">
-      {/* <h1>Olá Mundo</h1> */}
-      
       <textarea className="answer-box" id="answer-box" value={answer}></textarea>
       <textarea className="text-box" id="text-box" placeholder="Digite a pergunta" onKeyDown={(e) => handleSubmit(e)}></textarea>
     </div>
   );
-}
+};
 
 export default App;
